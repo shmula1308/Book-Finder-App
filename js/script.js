f085a7e897e82239fce4f3f35ecf834f5fdba0c9
Shpend Mula <shmula@hotmail.com>
2021-07-05 13:52:13 +0200

    completed library page html css

--- js/script.js
+++ js/script.js
@@ -8,3 +8,29 @@ searchMethodsContainer.addEventListener("click", (ev) => {
     ev.target.previousElementSibling.classList.add("checked");
   }
 });
+
+// const tagInput = document.querySelector(".tag-input");
+// function appendTags(tags) {
+//   document.querySelector('.tags').innerHTML = "";
+//   let df = new DocumentFragment()
+
+//   tags.map(tag => {
+//     let span = document.createElement('span');
+//     span.classList.add('tag');
+//     span.textContent = tag
+//     df.append(span)
+
+//   })
+//    document.querySelector('.tags').append(df)
+// }
+
+// tagInput.addEventListener('keyup', (ev)=> {
+//   if(ev.target.value.length === 0) {
+//     document.querySelector('.tags').innerHTML = "";
+//     return;
+//   }
+//     tags = ev.target.value.split(",")
+//     document.querySelector('.tags').innerHTML = "";
+//     appendTags(tags);
+//   CODE FOR TAG BUTTONS
+// })
