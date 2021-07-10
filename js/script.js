84791b1015dc8d45ce77c6a121954938845d924d
Shpend Mula <shmula@hotmail.com>
2021-07-10 15:21:01 +0200

    added functionality to edit book info

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ const app = {
     let page = document.body.id;
     switch (page) {
       case "librariesPage":
-        // app.getStore();
         app.displayStoredLibraries();
         app.displayStoredBooks();
         break;
@@ -130,31 +129,51 @@ const app = {
 
     let df = new DocumentFragment();
 
-    result.items.forEach((data) => {
+    result.items.forEach((data, i) => {
       let b = app.prepareBookData(data);
 
       let book = document.createElement("div");
       book.classList.add("book");
+      book.id = "book_" + i;
       book.innerHTML = `
-      <div class="book__thumbnail">
-        <img src=${b.thumbnail} width='140px' alt="Book Thumbnail">
-        <button class='btn btn--add-item'><i class="fas fa-plus-circle"></i>Add Item</button>
-      </div>
-      <div class="book__info">
-        <h4 class="book__title">${b.title}</h4>
-        <span class="book__author">${b.authors}</span>
-        <div class="book__data">
-          <span class="book__year">${b.datePublished}</span>
-          <span class="book__pages">${b.pages} pages</span>
-          <span class="book__publisher">${b.publisher}</span>
-          <span class="book__isbn10">ISBN-10: ${
-            !b.isbn[0] ? "no data" : b.isbn[0].identifier
-          }</span>
-          <span class="book__isbn13">ISBN-13: ${
-            !b.isbn[1] ? "no data" : b.isbn[1].identifier
-          }</span>
+      <form class="edit-form">
+        <input class="edit-form__input title-input" type="text" name="title" id=${
+          "title_" + i
+        } placeholder="Title">
+        <input class="edit-form__input author-input" type="text" name="authors" id=${
+          "author_" + i
+        } placeholder="Authors">
+        <small>Separate author(s) with comma.</small>
+        <textarea class="edit-form__input description-input" name="description" id=${
+          "description_" + i
+        } cols="30" rows="10" placeholder='Description'></textarea>
+        <small>shift + enter for newline</small>
+        <small>(press enter to save)</small>
+      </form>
+      <div class="inner-container">
+        <div class="book__thumbnail">
+          <img src=${b.thumbnail} width='140px' alt="Book Thumbnail">
+          <button class='btn btn--add-item'><i class="fas fa-plus-circle"></i>Add Item</button>
+          <button class="btn btn--delete-item" id=${
+            "deleteBook_" + i
+          }>Delete Item</button>
         </div>
-        <p class="book__description">${b.description}</p>
+        <div class="book__info">
+          <h4 class="book__title">${b.title}</h4>
+          <span class="book__author">${b.authors}</span>
+          <div class="book__data">
+            <span class="book__year">${b.datePublished}</span>
+            <span class="book__pages">${b.pages} pages</span>
+            <span class="book__publisher">${b.publisher}</span>
+            <span class="book__isbn10">ISBN-10: ${
+              !b.isbn[0] ? "no data" : b.isbn[0].identifier
+            }</span>
+            <span class="book__isbn13">ISBN-13: ${
+              !b.isbn[1] ? "no data" : b.isbn[1].identifier
+            }</span>
+          </div>
+          <p class="book__description">${b.description}</p>
+        </div>
       </div>`;
 
       df.append(book);
@@ -168,7 +187,7 @@ const app = {
 
   addBookToLibrary: (ev) => {
     if (ev.target.closest("button")) {
-      const addedBook = ev.target.parentElement.parentElement;
+      const addedBook = ev.target.parentElement.parentElement.parentElement;
       const book = {
         image: addedBook.querySelector("img").src,
         title: addedBook.querySelector(".book__title").textContent,
@@ -179,6 +198,7 @@ const app = {
         isbn10: addedBook.querySelector(".book__isbn10").textContent,
         isbn13: addedBook.querySelector(".book__isbn13").textContent,
         description: addedBook.querySelector(".book__description").textContent,
+        id: addedBook.id,
       };
 
       const selectedLibrary = JSON.parse(
@@ -201,8 +221,128 @@ const app = {
       });
 
       localStorage.setItem("libraries", JSON.stringify(updatedLibraries));
+      app.addControlsToStoredBook(addedBook.id);
     }
   },
+
+  addControlsToStoredBook: (storedBookId) => {
+    console.log(storedBookId);
+    const storedBook = document.getElementById(storedBookId);
+    const bookControls = document.createElement("div");
+    bookControls.classList.add("book__controls");
+    bookControls.innerHTML = `<i class="fas fa-ban" id="cancel-edit" style="display:none"></i>
+    <div class="controls-container">
+    <i class="fas fa-pencil-alt" title="Edit" id="edit"></i>
+    <i class="fas fa-tags" title="Tags" id="tags"></i>
+    <i class="fas fa-sticky-note" title="Add Notes" id="notes"></i>
+    <i class="fas fa-dollar-sign" title="Add Price" id="price"></i>
+    <i class="fas fa-cart-plus" title="Purchase" id="purchase"></i>
+    <i class="fas fa-trash-alt" title="Delete" id="delete"></i>
+    </div>
+    `;
+    storedBook.append(bookControls);
+
+    const addItemBtn = storedBook.querySelector(".btn--add-item");
+    addItemBtn.style.display = "none";
+    const deleteItemBtn = storedBook.querySelector(".btn--delete-item");
+    deleteItemBtn.style.display = "block";
+
+    deleteItemBtn.addEventListener("click", (ev) => {
+      console.log(ev.target);
+    });
+
+    bookControls.addEventListener("click", (ev) => {
+      if (ev.target.closest("i")) {
+        const action = ev.target.id;
+        app.editStoredBook(action, storedBookId);
+      } else {
+        return;
+      }
+    });
+  },
+
+  editStoredBook: (action, bookId) => {
+    switch (action) {
+      case "edit":
+        app.changeBookInfo(bookId);
+        break;
+      case "tags":
+        app.addBookTags(bookId);
+        break;
+      case "notes":
+        app.addBookNotes(bookId);
+        break;
+      case "price":
+        app.addBookPrice(bookId);
+        break;
+      case "purchase":
+        app.purchaseBook(bookId);
+        break;
+      case "delete":
+        app.deleteBook(bookId);
+        break;
+    }
+  },
+
+  changeBookInfo: (bookId) => {
+    app.toggleEditForm(bookId);
+    const book = document.getElementById(bookId);
+    const editForm = book.querySelector(".edit-form");
+    let titleText = book.querySelector(".book__title");
+    let authorText = book.querySelector(".book__author");
+    let descriptionText = book.querySelector(".book__description");
+
+    let titleInput = editForm.querySelector(".title-input");
+    titleInput.value = titleText.textContent;
+    let authorInput = editForm.querySelector(".author-input");
+    authorInput.value = authorText.textContent;
+    let descriptionInput = editForm.querySelector(".description-input");
+    descriptionInput.value = descriptionText.textContent;
+
+    editForm.addEventListener("keyup", (ev) => {
+      if (ev.code === "Enter") {
+        titleText.textContent = titleInput.value;
+        authorText.textContent = authorInput.value;
+        descriptionText.textContent = descriptionInput.value;
+        app.changeBookInfo(bookId); // Think about this! Doesnt look right even though it works
+      }
+    });
+  },
+
+  toggleEditForm: (bookId) => {
+    // Get book
+    const book = document.getElementById(bookId);
+    // Get edit form
+    const editForm = book.querySelector(".edit-form");
+    // Get book data container
+    const bookData = book.querySelector(".inner-container");
+    // Toggle on/off class for edit form
+    editForm.classList.toggle("add-edit-form");
+    // Toggle on/off class for book data container
+    bookData.classList.toggle("remove-book-data");
+
+    // book
+    //   .querySelector(".controls-container")
+    //   .classList.toggle("remove-controls");
+    // book.querySelector("#cancel-edit").style.display = "block";
+  },
+  addBookTags: (bookId) => {
+    console.log(bookId);
+  },
+  addBookNotes: (bookId) => {
+    console.log(bookId);
+  },
+  addBookPrice: (bookId) => {
+    console.log(bookId);
+  },
+  purchaseBook: (bookId) => {
+    console.log(bookId);
+  },
+  deleteBook: (bookId) => {
+    console.log(bookId);
+  },
+
+  removeControlsFromStoredBook: () => {},
 
   prepareBookData: (book) => {
     let bookObj = {
